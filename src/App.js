3835cb78b02ecf4361edd6258ab634a2b8a7358d
KKWANH <kwanho0096@gmail.com>
2024-01-10 15:41:03 +0100

    index update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return
-    <div>
-        Welcome to Team Slice!
-    </div>;
+  return <div>Welcome to Team Slice!</div>;
 }
 
 export default App;
